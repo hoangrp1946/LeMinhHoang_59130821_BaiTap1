0358b7f7826a23e3b20b49e0904760bdc46d4bff
hoangrp1946/LeMinhHoang_59130821_LifeCycle <hoangrp1946>
2020-04-16 19:25:29 +0700

    v1.3

---
+++ src/baitap1/BaiTap1Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap1;
+
+/**
+ *
+ * @author Pc
+ */
+public class BaiTap1Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LopHoc lh = new LopHoc();
+        HocSinh hs1 = new HocSinh("59CNTT1", "Thiết kế Web", "Mai Kim Trọng", 20, "Ninh Hòa", "0991234568");
+        HocSinh hs2 = new HocSinh("59CNTT3", "Tin học cơ sở", "Phạm Triệu Viễn", 21, "Bình Thuận", "093216547");
+        GiaoVien gv1 = new GiaoVien("Tin học cơ sở", "Công nghệ phần mềm", "Đoàn Vũ Thịnh", 34, "Nha Trang", "0902345678");
+        GiaoVien gv2 = new GiaoVien("Kỹ thuật đồ họa", "Công nghệ phần mềm", "Nguyễn Đình Hoàng Sơn", 38, "Nha Trang", "0981224589");       
+        lh.themHocSinh(hs1);
+        lh.themHocSinh(hs2);
+        lh.themGVGD(gv1);
+        lh.themGVGD(gv2);       
+        lh.inDSHS();
+        lh.inDSGVGD();
+    }
+    
+}
---
+++ src/baitap1/GiaoVien.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap1;
+
+/**
+ *
+ * @author Pc
+ */
+public class GiaoVien extends CaNhan{
+    String monDay;
+    String toBoMon;
+    
+    public GiaoVien(String monDay, String toBoMon, String hoTen, int tuoi, String diaChi, String sdt) {
+        super(hoTen, tuoi, diaChi, sdt);
+        this.monDay = monDay;
+        this.toBoMon = toBoMon;
+    }
+    @Override
+    public String hienThiTT() {
+        return super.hienThiTT()+"\n"+"Môn dạy: "+monDay+"\n" + "Tổ bộ môn: "+toBoMon+"\n"; 
+    }
+    
+}
